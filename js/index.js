b9f15a3c630c0fad0ee2951a48aa48f96bf9159c
PatriceBitonga <p.bitonga@gmail.com>
2022-07-30 23:20:31 -0600

    added toggle

--- js/index.js
+++ js/index.js
@@ -4,6 +4,12 @@ const menuBtn = document.querySelector('.menu-icon');
 const overlay = document.querySelector('.overlay');
 const menuBar = document.querySelector('.menu');
 const closeBtn = document.querySelector('.close-icon');
+const features = document.querySelector('.features');
+const company = document.querySelector('.company');
+const arrowBtnFeatures = document.querySelector('.arrow-btn1');
+const arrowBtnCompany = document.querySelector('.arrow-btn2');
+
+/* OPENING & CLOSING MENU BAR */
 
 menuBtn.addEventListener('click', () => {
     overlay.classList.remove('hidden');
@@ -12,4 +18,13 @@ menuBtn.addEventListener('click', () => {
 closeBtn.addEventListener('click', () => {
     overlay.classList.add('hidden');
     menuBar.classList.add('hidden');
+})
+
+/* OPENING & CLOSING FEATURES AND COMPANY BTN */
+
+arrowBtnFeatures.addEventListener('click', () => {
+    features.classList.toggle('features-nav');
+})
+arrowBtnCompany.addEventListener('click', () => {
+    company.classList.toggle('company-nav')
 })
